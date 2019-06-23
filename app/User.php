1bd280b35b421659d70715d220e9f02c8ac2d294
= <saeedissah@gmail.com>
2019-06-24 01:00:40 +0300

    Auth Repo

--- app/User.php
+++ app/User.php
@@ -2,15 +2,14 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
-    public static $USER_TYPES = ['admin','customer'];
+    public static $USER_TYPES = ['admin', 'customer'];
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +17,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
@@ -27,14 +28,29 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::created(function (User $user) {
+            if ($user->type === 'customer') {
+                $customer = new Customer();
+                $customer->user_id = $user->id;
+                $customer->phone = '';
+                $customer->save();
+            }
+        });
+    }
 
     /**
      * Returns the customer associated to the user or null if user is not a customer
      * @return \Illuminate\Database\Eloquent\Relations\HasOne | null
      */
-    public function customer(){
+    public function customer()
+    {
         return $this->hasOne(Customer::class);
     }
 
