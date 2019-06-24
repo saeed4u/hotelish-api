4cfd32ce88255196ab2ecb233fb13c8f22094741
= <=>
2019-06-24 20:27:19 +0300

    AuthRepo implmented

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repo\Auth\AuthRepoImpl;
+use App\Repo\AuthRepo;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(AuthRepo::class, AuthRepoImpl::class);
     }
 }
--- app/Repo/Auth/AuthRepoImpl.php
+++ app/Repo/Auth/AuthRepoImpl.php
@@ -9,7 +9,96 @@
 namespace App\Repo\Auth;
 
 
-class AuthRepoImpl
+use App\Repo\AuthRepo;
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
+
+class AuthRepoImpl implements AuthRepo
 {
 
+    /**
+     * @param $email
+     * @param $password
+     * @param string $api
+     * @return User
+     */
+    function login($email, $password, $api = ''): User
+    {
+        $auth = auth('api');
+        return $auth->attempt(['email' => $email, 'password' => $password]);
+    }
+
+    /**
+     * @param $email
+     * @param $password
+     * @return User
+     */
+    function register($email, $password): User
+    {
+        $user = new User();
+        $user->email = $email;
+        $user->password = $password;
+        if ($this->create($user)) {
+            return $user;
+        }
+        return null;
+    }
+
+    /**
+     * @param Model $model
+     * @return boolean
+     */
+    function create(Model $model): bool
+    {
+        return $model->save();
+    }
+
+    /**
+     * @param User $user
+     * @return bool
+     */
+    function logout(User $user): bool
+    {
+        $auth = auth('api');
+        $auth->logout();
+        return true;
+    }
+
+    /**
+     * @param User $user
+     * @return bool
+     */
+    function resetPassword(User $user): bool
+    {
+        //TODO: Not implemented
+        return false;
+    }
+
+    /**
+     * @param Builder $queryBuilder
+     * @return mixed
+     */
+    function read(Builder $queryBuilder)
+    {
+        return $queryBuilder->get();
+    }
+
+    /**
+     * @param Model $model
+     * @return bool
+     */
+    function update(Model $model): bool
+    {
+        return $model->update();
+    }
+
+    /**
+     * @param Model $model
+     * @return bool
+     */
+    function delete(Model $model): bool
+    {
+        return $model->delete();
+    }
 }
