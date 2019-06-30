559a6530b56d81413962942ef14891bdd8571d0a
= <=>
2019-06-30 22:50:22 +0300

    moved frontend to a separate repo

--- frontend/src/app/model/IErrorMessage.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by brasaeed on 26/06/2019.
- */
-export interface IErrorMessage {
-  title: string;
-  errorMessage: string;
-  errorDetail: any;
-}
--- frontend/src/app/model/ImageUploadResult.ts
+++
@@ -1,4 +0,0 @@
-import {Observable} from "rxjs/index";
-export interface FileUploadResult {
-  [key: string]: { progress: Observable<number> };
-}
