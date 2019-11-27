b22b39213ca0f428ab4b49b1b14b130e8aa8d332
Saeed <=>
2019-11-27 17:21:42 +0200

    Added device layer

--- database/migrations/2019_06_23_192149_create_countries_table.php
+++ database/migrations/2019_06_23_192149_create_countries_table.php
@@ -16,7 +16,7 @@ class CreateCountriesTable extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('iso_code')->unique();
+            $table->string('iso_code',5)->unique();
             $table->timestamps();
         });
     }
