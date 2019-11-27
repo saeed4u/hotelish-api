1b9e71fb4948df38c8bb1e74deacecc57c530388
Saeed <saeedissah@gmail.com>
2019-11-27 17:25:25 +0200

    Added device layer

--- database/migrations/2019_06_23_193056_create_rooms_table.php
+++ database/migrations/2019_06_23_193056_create_rooms_table.php
@@ -15,7 +15,7 @@ class CreateRoomsTable extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',200);
+            $table->string('name',200)->unique();
             $table->unsignedInteger('hotel_id');
             $table->unsignedInteger('room_type_id');
             $table->unsignedInteger('added_by')->nullable();
