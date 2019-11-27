7897c85b10b41d9fe6f89568bf38d1de3bf5f1f2
Saeed <=>
2019-11-28 00:07:43 +0200

    updates

--- database/seeds/BookingSeeder.php
+++ database/seeds/BookingSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Room;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,19 +17,22 @@ class BookingSeeder extends Seeder
         $startDate = now();
         $endDate = now()->addDays(7);
 
-        for ($i = 1; $i <= 10; $i++) {
-            $room = \App\Room::with('type.pricing')->find($i);
+        $user = \App\User::find(2);
+
+        for ($i = 1; $i <= 7; $i++) {
+            $room = Room::with('type.pricing')->find($i);
             $totalNights = $startDate->diffInDays($endDate);
             $pricing = $room->type->pricing;
             $booking = [
                 'room_id' => $i,
+                'user_id' => $user->id,
                 'pricing_id' => $pricing->id,
                 'start_date' => $startDate,
                 'end_date' => $endDate,
                 'total_nights' => $totalNights,
                 'total_price' => $pricing->price * $totalNights,
-                'customer_email' => "customer $i",
-                'customer_name' => "customer_$i@gmail.com",
+                'customer_email' => $user->email,
+                'customer_name' => $user->name,
             ];
             DB::table('bookings')->insert($booking);
         }
